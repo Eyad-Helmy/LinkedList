ca6c404b60734e56b34b1fa0ce89f89a016b2fc4
Eyad-Helmy <meayd8375@gmail.com>
2024-05-06 01:44:00 +0300

    added test run for a list of strings

--- main.cpp
+++ main.cpp
@@ -1,12 +1,22 @@
 #include <iostream>
+#include <string>
 #include "LinkedList.h"
 int main()
 {
+    std::cout << "List of integers: \n";
     LinkedList<int> list1;
-    int arr[5] = {1,2,3,4,5};
-    for(int element: arr) {
+    int arr[] = {1,2,3,4,5};
+    for(const int &element: arr) {
         list1.addToList(element);
     }
     list1.printlist();
+
+    std::cout<< "list of string: \n";
+    LinkedList<std::string> list2;
+    std::string arr2[] = {"Hello" , "world" , "this" , "a" , "test" , "run!"};
+    for(const std::string &element : arr2) {
+        list2.addToList(element);
+    }
+    list2.printlist();
     return 0;
 }
