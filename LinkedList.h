675b649c178600b7d0fdc4a5bbef08688ad0764b
Eyad-Helmy <meayd8375@gmail.com>
2024-05-05 22:56:46 +0300

    added node.h and header file for linked list class

---
+++ LinkedList.h
@@ -0,0 +1,8 @@
+//
+// Created by Eyad on 5/5/2024.
+//
+
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
+
+#endif //LINKEDLIST_H
---
+++ node.h
@@ -0,0 +1,8 @@
+//
+// Created by Eyad on 5/5/2024.
+//
+
+#ifndef NODE_H
+#define NODE_H
+
+#endif //NODE_H
